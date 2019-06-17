148a3059db2946b6d90a2082d1099edf5c42f724
AlexiaHdz <51898301+AlexiaHdz@users.noreply.github.com>
2019-06-16 23:13:48 -0500

    Add files via upload

---
+++ test_mymodule.py
@@ -0,0 +1,9 @@
+import unittest
+import mymodule
+
+class TestMyModule(unittest.TestCase):
+    
+    def test_sum(self):
+        self.assertEqual(mymodule.sum(5, 7), 12)
+if __name__ == "__main__":
+    unittest.main()
